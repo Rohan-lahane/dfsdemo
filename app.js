340bdafb9ca5d61088ac371b147d1b42b397bd81
rohan <rohanlahane30@gmail.com>
2023-10-02 14:53:55 +0530

    demo dfs app nagios

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3001;
+
+// Middleware to parse JSON request bodies
+app.use(bodyParser.json());
+
+// Define a route for the root path (GET request)
+app.get('/', (req, res) => {
+    res.send('Hello world to my demo dfs server');
+});
+
+
+app.post('/monitor', (req, res) => {
+    // Return a JSON object with the specified properties
+    const responseObject = {
+        message: 'Select service or host',
+        status: 1
+    };
+
+    res.json(responseObject);
+});
+
+// Define routes for /monitor/host and /monitor/services (POST requests)
+app.post('/monitor/host', (req, res) => {
+    // Log the received JSON data
+    console.log('Received data for /monitor/host:', req.body);
+
+    // Send the received JSON data back as-is
+    res.json(req.body);
+});
+
+app.post('/monitor/services', (req, res) => {
+    // Log the received JSON data
+    console.log('Received data for /monitor/services:', req.body);
+
+    // Send the received JSON data back as-is
+    res.json(req.body);
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
